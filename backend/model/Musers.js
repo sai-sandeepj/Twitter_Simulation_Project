31fb9e7bccf51d915eb6e09ccc757a5aedcc8943
KrishSJ <55265817+KrishSJ@users.noreply.github.com>
2019-11-18 13:24:26 -0800

    Add files via upload

---
+++ backend/model/Musers.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const followersSchema = require('./followers');
+const followingSchema = require('./following');
+
+const usersSchema = new Schema({
+    userName:{
+        type: String,
+        required: true
+    },
+    followers: [followersSchema],
+    following: [followingSchema],
+    bookmarked: [{
+        tweetId: {
+            type: mongoose.Schema.Types.ObjectId
+        }
+    }],
+    liked: [{
+        tweetId: {
+            type: mongoose.Schema.Types.ObjectId
+        }
+    }],
+    views: [{
+        viewDate: {
+            type: Date
+        },
+        viewCounter: {
+            type: Number
+        }
+    }],
+    retweets: [{
+        tweetId: {
+            type: mongoose.Schema.Types.ObjectId
+        },
+        retweetTime: {
+            type: Date
+        }
+    }]
+});
+
+module.exports = mongoose.model('Musers',usersSchema);
