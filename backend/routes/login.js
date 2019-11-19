a27b8d23e57ae921828fcccb873b2d859eac34f8
KrishSJ <55265817+KrishSJ@users.noreply.github.com>
2019-11-18 16:08:21 -0800

    Update login.js

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,1 +1,42 @@
+const express = require('express');
+const mySqlPool = require('../configFiles/MysqlConnectionPooling');
+var mysql = require('mysql');
 
+const app = express.Router();
+
+//login
+app.post('/login', async(req,res)=>{
+    console.log("In login post");
+    
+    mySqlPool.getConnection((err,conn)=>{
+        if(err){
+            console.log("Error while connecting to database");
+            res.writeHead(500,{
+                'Content-type': 'text/plain'
+            });
+            res.end("Error while connecting to database");
+        }else{
+            //query
+            const sql1 = `SELECT * FROM users where userEmail= ${mysql.escape(req.body.userEmail)}
+                            AND userPassword = ${mysql.escape(req.body.userPassword)}`;
+
+            conn.query(sql1, (err,result)=>{
+                if(err){
+                    res.writeHead(400,{
+                        'Contnet-type': 'text/plain'
+                    });
+                    res.end("Invalid credentials");
+                }else{
+                    console.log(result);
+                    res.end(""+result);
+                }
+            });
+        }
+    });
+    
+});
+
+
+
+
+module.exports = app;
