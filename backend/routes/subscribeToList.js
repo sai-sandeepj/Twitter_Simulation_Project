fc095aca9327d45ab53f069a511310b92dc081d2
nidhi07arvind <49506508+nidhi07arvind@users.noreply.github.com>
2019-11-28 12:52:22 -0800

    Retweet With and Without comment

--- backend/routes/subscribeToList.js
+++ backend/routes/subscribeToList.js
@@ -1,17 +1,24 @@
 const express = require("express");
 const mongoPool = require("../configFiles/MongoConnectionPooling");
 const app = express.Router();
-
+//
 const lists = require("../model/lists");
-
+var mongousers = require("../model/mongousers");
+//
+//
 app.post("/subscribeToList", async (req, res) => {
   console.log("In subscribeToList POST");
+
+  //var query_type = JSON.stringify(req.body.query);
+  //var query_type = JSON.stringify(req.body.query);
+  var query_type = req.body.query;
+  console.log("query:", query_type);
 
   return await lists
     .findOne({
       _id: req.body.listId
     })
-    .select("subscribers")
+    .select(req.body.query)
     .then(async result => {
       var subscribers_array = [];
 
@@ -43,6 +50,16 @@ app.post("/subscribeToList", async (req, res) => {
           .then(async result => {
             console.log("unsubscribed", result);
             res.end("unsubscribed", result);
+            //res.status(200).end(JSON.stringify(result));
+
+            var unsubscribed_list = {
+              _id: req.body.listId
+            };
+
+            return await mongousers.findOneAndUpdate(
+              { userName: req.body.userName },
+              { $pull: { query_type: unsubscribed_list } }
+            );
           })
           .catch(err => {
             console.log("");
@@ -51,9 +68,9 @@ app.post("/subscribeToList", async (req, res) => {
           });
       } else {
         var new_subscriber = {
-          userName: req.body.userName
-          // firstName: req.body.firstName,
-          // lastName: req.body.lastName,
+          userName: req.body.userName,
+          firstName: req.body.firstName,
+          lastName: req.body.lastName
           // //commentTime: Date.now(),
           // comment: req.body.comment,
           // commentMedia: req.body.commentMedia
@@ -63,7 +80,7 @@ app.post("/subscribeToList", async (req, res) => {
           .findByIdAndUpdate(
             req.body.listId,
             {
-              $push: { subscribers: new_subscriber }
+              $push: { query_type: new_subscriber }
             },
 
             { new: true }
@@ -71,6 +88,16 @@ app.post("/subscribeToList", async (req, res) => {
           .then(async result => {
             console.log("subscribed", result);
             res.end("subscribed", result);
+            // res.status(200).end(JSON.stringify(result));
+
+            var subscribed_list = {
+              _id: req.body.listId
+            };
+
+            return await mongousers.findOneAndUpdate(
+              { userName: req.body.userName },
+              { $push: { query_type: subscribed_list } }
+            );
           })
           .catch(err => {
             console.log("could not get likes");
