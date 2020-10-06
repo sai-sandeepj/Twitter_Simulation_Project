2ddd14c2d21084cec7b598225089adefe8932f69
Sai Sandeep Jyothula <saisandeep.jyothula@sjsu.edu>
2020-10-05 22:45:09 -0700

    Update MongoConnectionPooling.js

--- backend/configFiles/MongoConnectionPooling.js
+++ backend/configFiles/MongoConnectionPooling.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://nithin:nithin@grubhubtest-ly2ht.mongodb.net/273Twitter?retryWrites=true&w=majority',
+mongoose.connect('',
     { useNewUrlParser: true, useUnifiedTopology: true, poolSize: 100, 'useCreateIndex': true }, (err) => {
         if (err) {
             console.log("Could not connect to database", err);
