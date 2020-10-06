9e2e30af77758005120adbe2cd4c858c79b976fe
Sai Sandeep Jyothula <saisandeep.jyothula@sjsu.edu>
2020-10-05 22:44:49 -0700

    Update MysqlConnectionPooling.js

--- backend/configFiles/MysqlConnectionPooling.js
+++ backend/configFiles/MysqlConnectionPooling.js
@@ -1,15 +1,3 @@
-// const mysql = require('mysql');
-
-// var pool = mysql.createPool({
-//     connectionLimit: 100,
-//     host: 'localhost',
-//     user: 'root',
-//     password: '7777',
-//     database: 'twitter'
-// });
-
-// module.exports = pool;
-
 const mysql = require('mysql')
 
 const mySqlPool = mysql.createPool({
